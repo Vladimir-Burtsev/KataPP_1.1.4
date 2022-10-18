82987b3acf7534f87446d7556405734ebebb120c
vladimir burtsev <amakusl2@gmail.com>
2022-10-18 19:22:41 +0300

    parameterized query

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,8 +4,9 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
-import java.sql.SQLException;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -81,9 +82,9 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
     @Override
     public List<User> getAllUsers() {
         try (Session session = getSessionFactory().openSession()){
-            List<User> usersList = new ArrayList<>();
-            usersList = session.createQuery("FROM User").list();
-            return usersList;
+            TypedQuery<User> query =
+                    session.createQuery("SELECT w FROM User w", User.class);
+            return query.getResultList();
         }
     }
 
